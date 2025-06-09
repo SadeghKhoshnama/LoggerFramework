005b695a6266a28817e4d1de45310e99624dfd55
sadegh <skhoshnama77@gmail.com>
2025-06-10 00:33:16 +0330

    Fifth Commit Sadegh

--- src/core/LogManager.java
+++ src/core/LogManager.java
@@ -2,4 +2,9 @@ package core;
 
 public class LogManager {
 
+
+    public static Logger getLogger(String packageName,Level level){
+        return null;
+    }
+
 }
--- src/core/Main.java
+++ src/core/Main.java
@@ -2,7 +2,8 @@ package core;
 
 public class Main {
 
-    private static final Logger logger=LogManager.getLogger();
+    private static final Logger logger=
+            LogManager.getLogger("com.sadegh",Level.getLevel("INFO"));
     public static void main(String[] args) {
 
     }
