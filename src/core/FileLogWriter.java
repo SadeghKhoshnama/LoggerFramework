d0c7e5005c3c2fbc40e70720e81771ec4166495c
sadegh <skhoshnama77@gmail.com>
2025-06-27 14:16:27 +0330

    Mentor FileLogWriter is ready.

---
+++ src/core/FileLogWriter.java
@@ -0,0 +1,29 @@
+package core;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileLogWriter implements LogWriter{
+    private String filePath;
+    private final boolean append;
+
+    public FileLogWriter(String filePath,boolean append) {
+        this.filePath = filePath;
+        this.append=append;
+    }
+
+    @Override
+    public void write(String message) {
+
+        try (BufferedWriter bufferedWriter=new BufferedWriter(new FileWriter(filePath,true))){
+            bufferedWriter.write(message);
+            bufferedWriter.newLine();
+        }catch (IOException e){
+            System.err.println("Logging failed: " + e.getMessage());
+            //todo i will handle it later.
+        }
+
+    }
+}
