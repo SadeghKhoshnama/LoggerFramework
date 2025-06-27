715de3367d8475e35f9a761286e9dd7f3823e8d8
sadegh <skhoshnama77@gmail.com>
2025-06-27 13:57:04 +0330

    Time Formatter added to the project.

--- src/core/DefaultLogFormatter.java
+++ src/core/DefaultLogFormatter.java
@@ -1,11 +1,24 @@
 package core;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class DefaultLogFormatter implements LogFormatter{
+    private DateTimeFormatter dateTimeFormatter;
+
+    public DefaultLogFormatter() {
+        this.dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    }
+
+    public DefaultLogFormatter(DateTimeFormatter customeDateTimeFormatter) {
+        this.dateTimeFormatter=customeDateTimeFormatter!=null
+                ? customeDateTimeFormatter
+                : dateTimeFormatter;
+    }
 
     @Override
     public String format(Level level, String message, String packageName) {
-        return "["+ LocalDateTime.now() + "] ["+ level+"]"+ "["+ packageName +"] "+ message;
+        String timeStamp=LocalDateTime.now().format(dateTimeFormatter);
+        return "["+ timeStamp + "] ["+ level+"]"+ "["+ packageName +"] "+ message;
     }
 }
