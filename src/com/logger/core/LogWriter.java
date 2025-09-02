6a82a7d8048735020ee2924f6af4d72b84f54190
Sadegh <sadegh@Sadeghs-MacBook-Air.local>
2025-09-02 22:33:21 +0330

    first commit after throw it away

--- src/com/logger/core/LogWriter.java
+++
@@ -1,6 +0,0 @@
-package com.logger.core;
-
-//if we only have functions like this i would rather make this as an interface.
-public interface LogWriter {
-   void write(Level level,String message,String packageName);
-}
--- src/com/logger/core/Main.java
+++ src/com/logger/core/Main.java
@@ -1,16 +1,8 @@
 package com.logger.core;
 
 public class Main {
-    private static final Logger logger=LogManager.newLogger("com.logger.core","resources/config.properties");
-    public static void main(String[] args) {
-        LogManager.setLevel(logger.getPackageName(),Level.DEBUG);
-        logger.trace("Hello Im Sadegh");
-        logger.info("Hello Im Infoooooo");
-        logger.warn("Hello im Warnnnnnn");
-        logger.error("Hello Im Errorrrrrr");
-        logger.fatal("Hello Im Fatalllllll");
-        logger.debug("Hello Im Debugggggg");
-        logger.debug("Connection for database is Okay error code Is:  %d",200);
 
+    public static void main(String[] args) {
+        System.out.println("Hello world");
     }
 }
